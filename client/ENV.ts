597325c40514617eea7382083557e21481f191cc
sachin <sachinkumar59756@gmail.com>
2026-06-14 18:31:11 +0530

    fixed ENV

--- client/ENV.ts
+++ client/ENV.ts
@@ -1,9 +1,5 @@
-import dotenv from 'dotenv'
-dotenv.config();
-
 const ENV = {
-  VITE_API_URL:process.env.VITE_API_URL,
-  
+  VITE_API_URL: import.meta.env.VITE_API_URL,
 };
 
 export default ENV;
--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -1,6 +1,34 @@
 import { useRef, useState } from 'react'
 import { Editor, type OnMount } from '@monaco-editor/react'
-import LanguageSelect from './LanguageSelect'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import languages from '../constants'
+
+function LanguageSelect() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectGroup>
+          {Object.keys(languages).map((key) => (
+            <SelectItem key={key} value={key}>
+              {key}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
 
 function CodeEditor() {
   const editorRef = useRef<Parameters<OnMount>[0] | null>(null)
